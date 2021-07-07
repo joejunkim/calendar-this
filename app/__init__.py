f7e307af5055b5f21a648ec573fa9c65bd2163b4
Joe Kim <joekim6a@gmail.com>
2021-07-07 16:02:09 -0700

    Add new project files Pipfile and Pipfile.lock

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+# from app.config import Config
+# from .routes import workout_routes
+
+app = Flask(__name__)
+# app.config.from_object(Config)
+
+# @app.route('/')
+# def index():
+#     return "Go to /workouts to use the app!"
+
+# app.register_blueprint(workout_routes)
